fce064e68e8b967efd966f252ea13e9d9d7336ab
Sergey Ochkasov <sochkasov@gmail.com>
2015-03-27 11:32:16 +0300

    Update rabbitmq-status.sh

--- scripts/rabbitmq/rabbitmq-status.sh
+++ scripts/rabbitmq/rabbitmq-status.sh
@@ -23,5 +23,12 @@ fi
 
 # This assumes that the server is going to then use zabbix_sender to feed the data BACK to the server.  Right now, I'm doing that
 # in the python script
+# This assumes that the server is going to then use zabbix_sender to feed the data BACK to the server.  Right now, I'm doing that
+# in the python script
+# Отправка данных в Zabbix происходит при запросе с сервера какого-то параметра, например rabbitmq[queues]
+# При этом происходит получение от rabbitmq данных, которые укаковываются в файл в /tmp и отсылаются при помощи
+# zabbix-sender на сервер. Если данные от сервера не передаются более 5минут или происходит какая-либо ошибка при отправке данных
+# на сервер, то срабатывает триггер
+./api_new.py --hostname=$HOSTNAME --username=$USERNAME --password=$PASSWORD --check=$TYPE_OF_CHECK --metric=$METRIC --node="$NODE" --filters="$FILTER" --conf=$CONF   --zabbix_server=$ZABBIX_SERVER --senderhostname=$ZABBIX
 
-./api.py --hostname=$HOSTNAME --username=$USERNAME --password=$PASSWORD --check=$TYPE_OF_CHECK --metric=$METRIC --node="$NODE" --filters="$FILTER" --conf=$CONF
+
